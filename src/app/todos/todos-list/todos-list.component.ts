fc19eeba5e0173fc38843ec644c50506684b6a3b
Rafii <rafiiyoussef@gmail.com>
2019-12-23 22:59:35 +0100

    Adding contnent to the Todos Component

--- src/app/todos/todos-list/todos-list.component.ts
+++ src/app/todos/todos-list/todos-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo.model';
 
 @Component({
   selector: 'app-todos-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos-list.component.css']
 })
 export class TodosListComponent implements OnInit {
-
+  todos: Todo[] = [
+    new Todo('Learn Angular', 'Must Learn angular In 4 days', 'In Progress'),
+    new Todo('Learn Spring Cloud', 'Learn the spring cloud framework', 'Not Started'),
+    new Todo('Learn Java', 'Learn Java', 'Completed'),
+  ];
   constructor() { }
 
   ngOnInit() {
