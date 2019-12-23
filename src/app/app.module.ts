e2cdda10c0ba609f3cce733da82e165a2e7fa6ef
Rafii <rafiiyoussef@gmail.com>
2019-12-23 22:42:09 +0100

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodosListComponent } from './todos/todos-list/todos-list.component';
+import { TodosDetailComponent } from './todos/todos-detail/todos-detail.component';
+import { TodoItemComponent } from './todos/todos-list/todo-item/todo-item.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CatorgoryEditComponent } from './categories/catorgory-edit/catorgory-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TodosComponent,
+    TodosListComponent,
+    TodosDetailComponent,
+    TodoItemComponent,
+    CategoriesComponent,
+    CatorgoryEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/categories/catorgory-edit/catorgory-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catorgory-edit',
+  templateUrl: './catorgory-edit.component.html',
+  styleUrls: ['./catorgory-edit.component.css']
+})
+export class CatorgoryEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
