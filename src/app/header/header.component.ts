c2c148c527fca585c2ea601057081562c233ba27
Rafii <rafiiyoussef@gmail.com>
2019-12-24 11:58:01 +0100

    adding navigation by eventEmitter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() selectedPage = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectPage(page: string) {
+    this.selectedPage.emit(page);
   }
 
 }
